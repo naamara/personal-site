fdb217cfacd2ef74b5f1404580deb951e25ede9b
Hannington Naamara <naamarahannz@gmail.com>
2021-07-07 17:47:11 +0300

    New website for Hannington

---
+++ site3env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/acro/projects/Django-personal-website-master/site3env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
